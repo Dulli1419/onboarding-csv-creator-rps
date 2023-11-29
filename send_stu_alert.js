0b9250b202bd882b7ad74632171f94d83e4d429d
M <dulli1419@gmail.com>
2023-11-29 10:47:47 -0500

    update distribution lists

--- send_stu_alert.js
+++ send_stu_alert.js
@@ -146,9 +146,10 @@ function enrollAlertCompile() {
 		'msantowasso@rutgersprep.org',
 		'loy@rutgersprep.org',
 		'forte@rutgersprep.org',
+		'sullivan@rutgersprep.org',
 	]; // list of people that recieve the notificaiton regardless of division.
 	const usRecipient = ['ryan@rutgersprep.org', 'bautista-burk@rutgersprep.org']; // US only recipients
-	const msRecipient = ['mcmillen@rutgersprep.org', 'simpson@rutgersprep.org']; // MS only recipients
+	const msRecipient = ['mcmillen@rutgersprep.org', 'simpson@rutgersprep.org', 'marotto@rutgersprep.org']; // MS only recipients
 	const lsRecipient = ['tolia@rutgersprep.org']; // LS only recipients
 
 	// if there are US students, email the US.
